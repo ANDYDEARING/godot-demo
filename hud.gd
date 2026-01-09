039deeadff2d3c0bbcd7f29d5e74a68c8c8ec974
Andrew Dearing <andy.dearing@gmail.com>
2026-01-09 09:07:05 -0500

    fixed with deleting .godot folder

--- hud.gd
+++ hud.gd
@@ -18,7 +18,7 @@ func show_game_over():
 	
 	await $MessageTimer.timeout
 	
-	$Message.text = "Dodge the Creeps Again!"
+	$Message.text = "Dodge the Creeps (Fixed)!"
 	$Message.show()
 	
 	await get_tree().create_timer(1.0).timeout
